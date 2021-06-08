e4b3097a188b2780a88a250d393802b3df763232
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-06-08 17:34:47 +0300

    update deploy
    fix link volumes
    add docker-compose on script

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,11 @@ apt-get install wget
 
 wget "https://github.com/moodle/moodle/archive/refs/tags/v3.11.0.tar.gz"
 
-mkdir -p /opt/web{data,html}
+mkdir -p /opt/moodle/{database,data,html}
 cd /tmp && tar -xf v3.11.0.tar.gz
 mv moodle-3.11.0/. /opt/moodle/html/
 rm /tmp/v3.11.0.tar.gz
 
-docker build . -t moodle 
+docker build . -t moodle
 
-docker run -d -p 80:80 --name mdl -v /opt/moodle:/var/www/ moodle
-
-
-
+docker-compose up -d
