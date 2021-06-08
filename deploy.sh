db9acb24736b30e078b1acff634f23447f2cfdf4
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-06-09 02:56:36 +0300

    Update deploy.sh
    fix installation data

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-apt-get update -y 
-apt-get install wget
-
 wget "https://github.com/moodle/moodle/archive/refs/tags/v3.11.0.tar.gz"
 
-mkdir -p /opt/moodle/{data,html}
+mkdir -p /opt/moodle/html
 mkdir /opt/moodle-db
-cd /tmp && tar -xf v3.11.0.tar.gz
-mv moodle-3.11.0/. /opt/moodle/html/
+chmod 777 /opt/moodle
+tar -xf v3.11.0.tar.gz
+mv moodle-3.11.0/* /opt/moodle/html/
 rm /tmp/v3.11.0.tar.gz
 
 docker build . -t moodle
