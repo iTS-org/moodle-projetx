37a0bf0bd8bb78cdbcdd7b84b69a1d412c0ac18a
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-06-08 17:38:24 +0300

    Update deploy.sh
    create database dir

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ apt-get install wget
 
 wget "https://github.com/moodle/moodle/archive/refs/tags/v3.11.0.tar.gz"
 
-mkdir -p /opt/moodle/{database,data,html}
+mkdir -p /opt/moodle/{data,html}
+mkdir /opt/moodle-db
 cd /tmp && tar -xf v3.11.0.tar.gz
 mv moodle-3.11.0/. /opt/moodle/html/
 rm /tmp/v3.11.0.tar.gz
