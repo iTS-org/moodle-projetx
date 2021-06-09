f081fc6835b3a15cac1a5b06b238d39d8aa1ed60
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-06-09 03:21:57 +0300

    Update deploy.sh
    access write to avoid copy file

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ wget "https://github.com/moodle/moodle/archive/refs/tags/v3.11.0.tar.gz"
 
 mkdir -p /opt/moodle/html
 mkdir /opt/moodle-db
-chmod 777 /opt/moodle
+chmod 777 -R /opt/moodle
 tar -xf v3.11.0.tar.gz
 mv moodle-3.11.0/* /opt/moodle/html/
 rm /tmp/v3.11.0.tar.gz
